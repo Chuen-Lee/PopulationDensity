119efc27b03b2d202e7c02e5cbf675b98f60cc42
Chuen <92123911+Chuen-Lee@users.noreply.github.com>
2025-04-28 15:47:27 +0100

    Scripts to make plots of population density of SW on Cousin
    Make sure you update the data file (PopDenCN.tsv) before running

---
+++ PopulationDensityCN_CL_20250428.R
@@ -0,0 +1,19 @@
+# Population density plot for Cousin - Chuen 28/04/2025
+
+PopDenCN <- read_tsv("~/Documents/PhD/R_analysis/Weather/PopDenCN.tsv") # update PopDenCN.tsv and make sure path is correct
+
+PopDenCNplot <- ggplot(PopDenCN, aes(PEnd,IndependentBirds)) + # x = End of FP, y=Independent birds
+  geom_point(aes(colour=Pcolor)) + # colour by the most recent season
+  geom_line()  + 
+  xlab("Period End Date") + 
+  ylab("Number of Independent Birds Observed") + 
+  ylim(21,450) + 
+  scale_x_date(limits=c(as.Date( "1997-01-01", "%Y-%m-%d"), 
+                        as.Date("2025-04-01", "%Y-%m-%d")), 
+               date_breaks = "5 years", date_labels = "%Y") + # Limit the x-axis and split by 5 year period - make sure to change the numbers
+  theme_tufte(base_size = 15, base_family = "Arial") + 
+  theme(axis.line = element_line(colour = "black", linetype=1)) +
+  scale_color_manual(values = c("black","red")) +
+  theme(legend.title=element_blank())
+
+print(PopDenCNplot)
